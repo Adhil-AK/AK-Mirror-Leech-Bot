ae77e5ddaa1a525679ecd37af8f234f29a9b68eb
akakmacnt@gmail.com <akakmacnt@gmail.com>
2022-07-19 21:43:15 +0530

    .

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -160,7 +160,7 @@ def get_readable_message():
                     msg += f"\n<b>├ Extracted:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 elif download.status() == MirrorStatus.STATUS_SPLITTING:
                     msg += f"\n<b>├ Splitted:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>├ Speed:</b> {download.speed()}\n<b>Waiting Time:</b> {download.eta()}"
+                msg += f"\n<b>├ Speed:</b> {download.speed()}\n<b>├ ETA:</b> {download.eta()}"
                 msg += f"\n<b>├ Elapsed : </b>{get_readable_time(time() - download.message.date.timestamp())}"
                 msg += f'\n<b>├ Req By :</b> <a href="https://t.me/c/{str(download.message.chat.id)[4:]}/{download.message.message_id}">{download.message.from_user.first_name}</a>'
                 msg += f"\n<b>├ Engine :</b> {download.eng()}"
